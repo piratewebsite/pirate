010c84837d8ec84836574b378fa7e1f78143c8fc
Todd <ihatespam@gmail.com>
2025-12-30 03:24:35 -0600

    astro hybrid test

--- astro.config.ts
+++ astro.config.ts
@@ -17,7 +17,7 @@ export const reader = createReader(process.cwd(), keystaticConfig);
 
 const isVercel = !!process.env.VERCEL;
 const adapter = isVercel ? vercel() : netlify({ edgeMiddleware: false });
-const output: 'static' | 'server' = 'static'; 
+const output: 'static' | 'server' | 'hybrid' = 'hybrid'; 
 
 const pwaSettings = await reader.singletons.pwaSettings.read();
 
